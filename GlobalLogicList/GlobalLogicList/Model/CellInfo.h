ef232837302cc3566dce12d31b28d97293452b47
Nicolás Fernández <nicolas.fernandez.a@gmail.com>
2018-06-11 14:36:02 -0400

    Added object to map cell information

---
+++ GlobalLogicList/GlobalLogicList/Model/CellInfo.h
@@ -0,0 +1,17 @@
+//
+//  CellInfo.h
+//  GlobalLogicList
+//
+//  Created by Nicolás Fernández Alarcón on 11-06-18.
+//  Copyright © 2018 Nicolás Fernández Alarcón. All rights reserved.
+//
+
+#import <Mantle/Mantle.h>
+
+@interface CellInfo : MTLModel <MTLJSONSerializing>
+
+@property (nonatomic, copy, readonly) NSString *title;
+@property (nonatomic, copy, readonly) NSString *descriptionInfo;
+@property (nonatomic, copy, readonly) NSURL *image;
+
+@end
---
+++ GlobalLogicList/GlobalLogicList/Model/CellInfo.m
@@ -0,0 +1,17 @@
+//
+//  CellInfo.m
+//  GlobalLogicList
+//
+//  Created by Nicolás Fernández Alarcón on 11-06-18.
+//  Copyright © 2018 Nicolás Fernández Alarcón. All rights reserved.
+//
+
+#import "CellInfo.h"
+
+@implementation CellInfo
+
++ (NSDictionary *)JSONKeyPathsByPropertyKey { 
+	return [NSDictionary mtl_identityPropertyMapWithModel:self];
+}
+
+@end
