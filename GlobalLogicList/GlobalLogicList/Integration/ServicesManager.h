027485b7cc1b87e283438be242462922b49e3e36
Nicolás Fernández <nicolas.fernandez.a@gmail.com>
2018-06-11 15:19:44 -0400

    Add service manager class

---
+++ GlobalLogicList/GlobalLogicList/Integration/ServicesManager.h
@@ -0,0 +1,13 @@
+//
+//  ServicesManager.h
+//  GlobalLogicList
+//
+//  Created by Nicolás Fernández Alarcón on 11-06-18.
+//  Copyright © 2018 Nicolás Fernández Alarcón. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ServicesManager : NSObject
+
+@end
---
+++ GlobalLogicList/GlobalLogicList/Integration/ServicesManager.m
@@ -0,0 +1,13 @@
+//
+//  ServicesManager.m
+//  GlobalLogicList
+//
+//  Created by Nicolás Fernández Alarcón on 11-06-18.
+//  Copyright © 2018 Nicolás Fernández Alarcón. All rights reserved.
+//
+
+#import "ServicesManager.h"
+
+@implementation ServicesManager
+
+@end
